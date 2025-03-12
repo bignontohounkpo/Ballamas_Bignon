f042e65b4d69fb8a3af0769c5c3866e1d84746d7
bignontohounkpo <bpriscatohounkpo@gmail.com>
2025-03-12 15:40:50 +0100

    update

--- src/Pages/Details.vue
+++ src/Pages/Details.vue
@@ -1,9 +1,11 @@
 <template>
   <CardDetail></CardDetail>
+  <Like></Like>
 </template>
 
 <script setup>
 import CardDetail from '../components/Details/CardDetail.vue';
+import Like from '../components/Details/Like.vue';
 </script>
 
 <style>
---
+++ src/components/Details/Like.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="container">
+    <div class="font-chillax place-items-center">
+        <h4 class="font-semibold">
+            You like make also 
+        </h4>
+    </div>
+   </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
